5983e448f680e4052ced5c5aa110785dc1c3f4f2
Gabriel Nascimento <gabriel.nascimenton.19@gmail.com>
2020-01-17 22:50:13 -0300

    adicionando a responsividade

--- src/components/Tabela/index.js
+++ src/components/Tabela/index.js
@@ -20,17 +20,18 @@ class Tabela extends Component {
             <>
                 <div className="corpo">
                     <div className="pai">
+                            <h2 className="textLogo">ShareMe</h2>
                         <div className="public">
                             <h4>Publicar menssagem</h4>
                             <small className="my-3">escreva uma publicaçõ e compartilhe em uma das redes sociais abaixo</small>
                             <textarea rows="5" placeholder="No que você esta pensando ?" value={coment} onChange={setComent} />
                         </div>
                         <hr className="my2" />
-                        <h5>Compartilhe com suas redes</h5>
+                        <h5>Compartilhe em suas redes</h5>
                         <div className="sharer">
                             <a href={`https://api.whatsapp.com/send?text=${coment}`} class="btn text-success"> <FontAwesomeIcon icon={faWhatsapp} size="1x"></FontAwesomeIcon>Compartilhar</a>
-                            <a href={`https://facebook.com/sharer/sharer.php?u=www.verzel.com.br&quote=${coment}`} class=" btn text-primary ml-5    "><FontAwesomeIcon icon={faFacebook} size="1x"></FontAwesomeIcon>Compartilhar </a>
-                            <a href={`https://twitter.com/intent/tweet?url=www.verzel.com.br&text=${coment}`} class=" btn text-primary ml-5 "><FontAwesomeIcon icon={faTwitter} size="1x"></FontAwesomeIcon>Compartilhar</a>
+                            <a href={`https://facebook.com/sharer/sharer.php?u=www.verzel.com.br&quote=${coment}`} class=" btn text-primary     "><FontAwesomeIcon icon={faFacebook} size="1x"></FontAwesomeIcon>Compartilhar </a>
+                            <a href={`https://twitter.com/intent/tweet?url=www.verzel.com.br&text=${coment}`} class=" btn text-primary  "><FontAwesomeIcon icon={faTwitter} size="1x"></FontAwesomeIcon>Compartilhar</a>
                         </div>
                     </div>
                 </div>
